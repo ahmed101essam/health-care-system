25fb6830052b764bd046111d82441bd521eb785d
ahmed101essam <ahmed10pepeto@gmail.com>
2025-04-13 02:37:20 +0200

    adding controllers of error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -13,6 +13,8 @@ const handleValidationError = (error) => {
 module.exports = (err, req, res, next) => {
   console.log(err);
 
+  console.log("error controller");
+
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
