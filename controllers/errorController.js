29816f4ef0683b93f06ae2b7575ad516398d247e
ahmed101essam <ahmed10pepeto@gmail.com>
2025-04-13 02:26:42 +0200

    adding controllers of error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,28 @@
+const AppError = require("../utils/AppError");
+
+const handleValidationError = (error) => {
+  const errors = Object.values(error.errors).map((e) => {
+    return e.message;
+  });
+
+  const message = `Invalid input data. ${errors.join(". ")}`;
+
+  return new AppError(message, 400);
+};
+
 module.exports = (err, req, res, next) => {
   console.log(err);
 
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err,
+  let error = { ...err };
+  err.message = err.message;
+  if (err.name === "ValidationError") {
+    error = handleValidationError(error);
+  }
+  res.status(error.statusCode).json({
+    status: error.status,
+    message: error.message,
   });
 };
